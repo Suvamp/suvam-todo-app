003c093cf23ffe50288f6b5a2a11db280e1f4327
Suvamp <patelsuvam621@gmail.com>
2023-06-30 14:34:18 -0700

    Add todos and web app

--- Apps/App_1/App_1_gui.py
+++ Apps/App_1/App_1_gui.py
@@ -1,6 +1,11 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
 
 
 sg.theme("DarkBlue1")
@@ -8,7 +13,8 @@ sg.theme("DarkBlue1")
 clock = sg.Text("", key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(size=2, image_source="add.png", mouseover_colors="LightBlue2",
+                       tooltip="Add Todo", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
@@ -29,6 +35,7 @@ while True:
     print(2, values)
     print(3, values['todos'])
     match event:
+
         case "Add":
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
